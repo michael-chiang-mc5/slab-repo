4209ad46b1f0eee12d38bdfcd8f752669e4bdc7b
michael <michael.chiang.mc5@gmail.com>
2016-09-26 23:50:07 -0700

    First real commit

---
+++ mySite/ImagePicker/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
+# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'ImagePicker/index4.html',context)
+
+def save_image(request,latitude,longitude,heading,pitch):
+    s = StreetviewImage(latitude=latitude, longitude=longitude, heading=heading, pitch=pitch)
+    s.save()
+    return HttpResponse('done')
+
+def list(request):
+    streetviewImages = StreetviewImage.objects.all()
+    context = {'streetviewImages':streetviewImages}
+    return render(request, 'ImagePicker/list.html', context)
---
+++ mySite/Localizer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LocalizerConfig(AppConfig):
+    name = 'Localizer'
